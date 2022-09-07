9eaf544c0285def91dc10a5df2a35f94238e0c3f
cezufah <cezufah@gmail.com>
2022-09-07 11:26:18 -0700

    done

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -10,5 +10,12 @@ public class GettingHotInHere {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+        System.out.println(number + "°F");
+        double cels = ((number - 32.0) * (5.0/9.0));
+        System.out.println(cels + "°C");
+        System.out.printf("%.0f", cels);
+        System.out.println("°C");
+        System.out.println("If it were 2°C warmer it would be: ");
+        System.out.println(cels + 2 + "°C");
     }
 }
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -15,5 +15,7 @@ public class MrRoboto {
 
         System.out.print("My name is Mr. Roboto, are you Kilroy? ");
         // Write your program here
+        String line = scanner.nextLine();
+        System.out.printf(domo + line);
     }
 }
